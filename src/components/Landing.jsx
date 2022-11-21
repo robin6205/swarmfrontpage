f2b56ae8687405001959e57993a5a8611c4b686d
robin6205 <robin6205@gmail.com>
2022-11-22 06:27:26 +0900

    button with subtitle

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,20 +4,28 @@ import demoVideo from '../video/map3-3short.mp4'
 
 function Landing() {
   return (
-  
+    <div className="landing">
     <div className='video-background'>
     <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap');
-</style>
+      @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap');
+    </style>
     <h1 className='title'>SWARM Simulation Platform</h1>
+    <h3 className='subtitle'>A platform for simulating and visualizing swarm robotics</h3>
     <div className="gradient-overlay">
-    {/* <video className='video-foreground' autoPlay loop muted>
+    <video className='video-foreground' autoPlay loop muted>
       <source src={demoVideo} type='video/mp4' />
-    </video> */}
-    <video className='video-foreground' src={demoVideo} autoPlay loop muted />
+    </video>
+    {/* <video className='video-foreground' src={demoVideo} autoPlay loop muted /> */}
     </div>  
-    
     </div>
+    <button className='startbutton' color="#f194ff" onClick={() => console.log('Simple Button pressed')}>Get Started</button>
+    
+    
+
+
+    </div>
+    
+    
   )
 }
 
